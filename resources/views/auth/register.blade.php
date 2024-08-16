593007f547d96dd1380b47dac5c41aaf3754339d
Ld28Fax <fandresenarasolomaminirina@gmail.com>
2024-08-16 12:14:56 +0300

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <!-- <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
@@ -26,7 +26,7 @@
                             required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+        </div> -->
 
         <!-- Confirm Password -->
         <div class="mt-4">
